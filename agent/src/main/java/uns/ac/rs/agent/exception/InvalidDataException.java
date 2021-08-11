030b7b525205e40559ad536548233701a245107b
nikolina97 <41080295+nikolina97@users.noreply.github.com>
2021-08-11 22:19:51 +0200

    Develop (#12)
    * #1 - Create base CI pipeline
    
    * #2 - add new prod (#5)
    
    * #2 - add new prod
    
    * ci updated
    
    * ci updated2
    
    * ci updated3
    
    * bug fixed
    
    * bug fixed2
    
    * test added
    
    * poruka
    
    * dsda
    
    * 11233213
    
    * qaqewqe
    
    * Feature/#2 init project (#6)
    
    * #2 - add new prod
    
    * ci updated
    
    * ci updated2
    
    * ci updated3
    
    * bug fixed
    
    * bug fixed2
    
    * test added
    
    * poruka
    
    * dsda
    
    * 11233213
    
    * qaqewqe
    
    * ohoho
    
    * Add product #3 (#7)
    
    * Feature/push pipeline (#8)
    
    * Add product #3
    
    * pipeline updated
    
    * pipeline updatedds
    
    * pipeline updateddsdsadsad
    
    * #9 Push pipeline added (#10)
    
    * Feature/#9 develop pipeline (#11)
    
    * #9 Push pipeline added
    
    * pom xml changed
    
    * 12321451
    
    * docker push
    
    * 123
    
    * 332
    
    * 332
    
    * 332
    
    * 1233123
    
    * maven image added
    
    * maven image added2
    
    * main realease added

---
+++ agent/src/main/java/uns/ac/rs/agent/exception/InvalidDataException.java
@@ -0,0 +1,14 @@
+package uns.ac.rs.agent.exception;
+
+public class InvalidDataException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidDataException() {
+		super();
+	}
+	
+	public InvalidDataException(String message) {
+		super(message);
+	}
+}
---
+++ agent/src/test/java/uns/ac/rs/agent/integration/ProductIT.java
@@ -0,0 +1,46 @@
+package uns.ac.rs.agent.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import uns.ac.rs.agent.domain.Product;
+import uns.ac.rs.agent.dto.ProductDTO;
+import uns.ac.rs.agent.exception.InvalidDataException;
+import uns.ac.rs.agent.mapper.ProductMapper;
+import uns.ac.rs.agent.repository.ProductRepository;
+import uns.ac.rs.agent.service.ProductService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProductIT {
+
+	@Autowired
+    private TestRestTemplate restTemplate;
+	
+	@Autowired
+	private ProductService productService;
+	
+	@MockBean
+    private ProductRepository productRepository;
+	
+	@Test
+	public void createProduct() throws InvalidDataException {
+		ProductDTO prodDTO = new ProductDTO("name", 200.20, 100, "picture");
+		Product prod = ProductMapper.toEntity(prodDTO);
+		when(productRepository.findOneByName(prodDTO.getName())).thenReturn(Optional.empty());	
+		when(productRepository.save(Mockito.any(Product.class))).thenReturn(prod);	
+		assertEquals(prod, productService.createProduct(prodDTO));
+	}
+}
